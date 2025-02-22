1b8aee66c572b1092ebd860b966e5d0a43161db9
Polina Muravieva <polinamur3@gmail.com>
2025-02-22 21:11:38 +0300

    Added validation for registration form fields

--- src/app/views/user/signup/signup.component.ts
+++ src/app/views/user/signup/signup.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, Validators} from "@angular/forms";
+import {MatSnackBar} from "@angular/material/snack-bar";
 
 @Component({
   selector: 'app-signup',
@@ -7,12 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  signupForm = this.fb.group({
+    name: ['', [Validators.required, Validators.pattern('^([А-ЯЁ][а-яё]*)(\\s[А-ЯЁ][а-яё]*)*$')]],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.pattern('^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$')]],
+    agree: [false, [Validators.requiredTrue]],
+  })
+
+  constructor(private fb: FormBuilder,
+              private _snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
   }
 
   signup() {
-
+    if (this.signupForm.valid && this.signupForm.value.name && this.signupForm.value.email
+      && this.signupForm.value.password && this.signupForm.value.agree) {
+      console.log('проверка прошла');
+    } else {
+      this.signupForm.dirty;
+    }
   }
 }
