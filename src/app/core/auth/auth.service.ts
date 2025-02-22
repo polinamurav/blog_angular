494979b0d6c1889a9e5c95090a5615cfffd85b45
Polina Muravieva <polinamur3@gmail.com>
2025-02-22 21:26:52 +0300

    Added POST request for registration form submission

---
+++ src/app/core/auth/auth.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {DefaultResponseType} from "../../../assets/types/default-response.type";
+import {LoginResponseType} from "../../../assets/types/login-response.type";
+import {environment} from "../../../environments/environment";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  public accessTokenKey: string = 'accessToken';
+  public refreshTokenKey: string = 'refreshToken';
+  public userIdKey: string = 'userId';
+
+  constructor(private http: HttpClient) { }
+
+  login(email: string, password: string, rememberMe: boolean): Observable<DefaultResponseType | LoginResponseType> {
+    return this.http.post<DefaultResponseType | LoginResponseType>(environment.api + 'login', {
+      email, password, rememberMe
+    });
+  }
+
+  signup(name: string, email: string, password: string): Observable<DefaultResponseType | LoginResponseType> {
+    return this.http.post<DefaultResponseType | LoginResponseType>(environment.api + 'signup', {
+      name, email, password
+    });
+  }
+
+  setTokens(accessToken: string, refreshToken: string) {
+    localStorage.setItem(this.accessTokenKey, accessToken);
+    localStorage.setItem(this.refreshTokenKey, refreshToken);
+  }
+
+  removeTokens(): void {
+    localStorage.removeItem(this.accessTokenKey);
+    localStorage.removeItem(this.refreshTokenKey);
+  }
+
+  getTokens(): {accessToken: string | null, refreshToken: string | null} {
+    return {
+      accessToken: localStorage.getItem(this.accessTokenKey),
+      refreshToken: localStorage.getItem(this.refreshTokenKey)
+    };
+  }
+
+  set userId(id: string | null) {
+    if (id) {
+      localStorage.setItem(this.userIdKey, id);
+    } else {
+      localStorage.removeItem(this.userIdKey);
+    }
+  }
+}
--- src/app/views/user/signup/signup.component.ts
+++ src/app/views/user/signup/signup.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, Validators} from "@angular/forms";
 import {MatSnackBar} from "@angular/material/snack-bar";
+import {AuthService} from "../../../core/auth/auth.service";
+import {HttpErrorResponse} from "@angular/common/http";
+import {DefaultResponseType} from "../../../../assets/types/default-response.type";
+import {LoginResponseType} from "../../../../assets/types/login-response.type";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-signup',
@@ -17,7 +22,9 @@ export class SignupComponent implements OnInit {
   })
 
   constructor(private fb: FormBuilder,
-              private _snackBar: MatSnackBar) { }
+              private _snackBar: MatSnackBar,
+              private authService: AuthService,
+              private router: Router) { }
 
   ngOnInit(): void {
   }
@@ -25,9 +32,37 @@ export class SignupComponent implements OnInit {
   signup() {
     if (this.signupForm.valid && this.signupForm.value.name && this.signupForm.value.email
       && this.signupForm.value.password && this.signupForm.value.agree) {
-      console.log('проверка прошла');
-    } else {
-      this.signupForm.dirty;
+      this.authService.signup(this.signupForm.value.name, this.signupForm.value.email, this.signupForm.value.password)
+        .subscribe({
+          next: (data: DefaultResponseType | LoginResponseType) => {
+            let error = null;
+            if ((data as DefaultResponseType).error !== undefined) {
+              error = (data as DefaultResponseType).message;
+            }
+
+            const loginResponse = data as LoginResponseType;
+            if (!loginResponse.accessToken || !loginResponse.refreshToken || !loginResponse.userId) {
+              error = 'Ошибка регистрации';
+            }
+
+            if (error) {
+              this._snackBar.open(error);
+              throw new Error(error);
+            }
+
+            this.authService.setTokens(loginResponse.accessToken, loginResponse.refreshToken);
+            this.authService.userId = loginResponse.userId;
+            this._snackBar.open('Вы успешно зарегистрировались');
+            this.router.navigate(['/']);
+          },
+          error: (errorResponse: HttpErrorResponse) => {
+            if (errorResponse.error && errorResponse.error.message) {
+              this._snackBar.open(errorResponse.error.message);
+            } else {
+              this._snackBar.open('Ошибка регистрации');
+            }
+          }
+        })
     }
   }
 }
