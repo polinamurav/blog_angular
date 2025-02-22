1ae2c8d85563884612136ea2662b974c192a24e2
Polina Muravieva <polinamur3@gmail.com>
2025-02-22 21:33:20 +0300

    Added POST request for login form submission

--- src/app/views/user/login/login.component.ts
+++ src/app/views/user/login/login.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, Validators} from "@angular/forms";
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {AuthService} from "../../../core/auth/auth.service";
+import {Router} from "@angular/router";
+import {DefaultResponseType} from "../../../../assets/types/default-response.type";
+import {LoginResponseType} from "../../../../assets/types/login-response.type";
+import {HttpErrorResponse} from "@angular/common/http";
 
 @Component({
   selector: 'app-login',
@@ -7,12 +14,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.pattern('^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$')]],
+    rememberMe: [false],
+  })
+
+  constructor(private fb: FormBuilder,
+              private _snackBar: MatSnackBar,
+              private authService: AuthService,
+              private router: Router) { }
 
   ngOnInit(): void {
   }
 
   login() {
+    if (this.loginForm.valid && this.loginForm.value.email && this.loginForm.value.password) {
+      this.authService.login(this.loginForm.value.email, this.loginForm.value.password, !!this.loginForm.value.rememberMe)
+        .subscribe({
+          next: (data: DefaultResponseType | LoginResponseType) => {
+            let error = null;
+            if ((data as DefaultResponseType).error !== undefined) {
+              error = (data as DefaultResponseType).message;
+            }
 
+            const loginResponse = data as LoginResponseType;
+            if (!loginResponse.accessToken || !loginResponse.refreshToken || !loginResponse.userId) {
+              error = 'Ошибка авторизации';
+            }
+
+            if (error) {
+              this._snackBar.open(error);
+              throw new Error(error);
+            }
+
+            this.authService.setTokens(loginResponse.accessToken, loginResponse.refreshToken);
+            this.authService.userId = loginResponse.userId;
+            this._snackBar.open('Вы успешно авторизировались');
+            this.router.navigate(['/']);
+          },
+          error: (errorResponse: HttpErrorResponse) => {
+            if (errorResponse.error && errorResponse.error.message) {
+              this._snackBar.open(errorResponse.error.message);
+            } else {
+              this._snackBar.open('Ошибка авторизации');
+            }
+          }
+        })
+    }
   }
 }
