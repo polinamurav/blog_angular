816b64ab05a7b3e8684e697305728341f7fc9179
Polina Muravieva <polinamur3@gmail.com>
2025-02-26 15:24:32 +0300

    add section contacts

--- src/app/views/main/main.component.ts
+++ src/app/views/main/main.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {OwlOptions} from "ngx-owl-carousel-o";
 import {ProductType} from "../../../assets/types/product.type";
 import {ProductService} from "../../shared/services/product.service";
@@ -32,6 +32,29 @@ export class MainComponent implements OnInit {
     },
   };
 
+  customOptionsReviews: OwlOptions = {
+    loop: true,
+    mouseDrag: false,
+    touchDrag: false,
+    pullDrag: false,
+    margin: 26,
+    dots: false,
+    navSpeed: 700,
+    navText: ['', ''],
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 2
+      },
+      740: {
+        items: 3
+      },
+    },
+    nav: false
+  };
+
   services = [
     {
       image: 'service1.png',
@@ -58,9 +81,33 @@ export class MainComponent implements OnInit {
       price: '750',
     },
   ];
+
+  reviews = [
+    {
+      image: 'review1.png',
+      name: 'Станислав',
+      text: 'Спасибо огромное АйтиШторму за прекрасный блог с полезными статьями! Именно они и побудили меня углубиться в тему SMM и начать свою карьеру.'
+    },
+    {
+      image: 'review2.png',
+      name: 'Алёна',
+      text: 'Обратилась в АйтиШторм за помощью копирайтера. Ни разу ещё не пожалела! Ребята действительно вкладывают душу в то, что делают, и каждый текст, который я получаю, с нетерпением хочется выложить в сеть.'
+    },
+    {
+      image: 'review3.png',
+      name: 'Мария',
+      text: 'Команда АйтиШторма за такой короткий промежуток времени сделала невозможное: от простой фирмы по услуге продвижения выросла в мощный блог о важности личного бренда. Класс!'
+    },
+    {
+      image: 'review4.jpg',
+      name: 'Аделина',
+      text: 'Хочу поблагодарить всю команду за помощь в создании блога!'
+    },
+  ]
   products: ProductType[] = [];
 
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService) {
+  }
 
   ngOnInit(): void {
     this.productService.getPopularProducts()
