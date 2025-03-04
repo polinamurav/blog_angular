48f907fe488d5794f6e4eec9e630bf3af5d050ac
Polina Muravieva <polinamur3@gmail.com>
2025-03-04 16:33:12 +0300

    popup for footer

--- src/app/shared/layout/footer/footer.component.ts
+++ src/app/shared/layout/footer/footer.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, Validators} from "@angular/forms";
+import {RequestService} from "../../services/request.service";
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {RequestType} from "../../../../assets/types/request.type";
+import {DefaultResponseType} from "../../../../assets/types/default-response.type";
 
 @Component({
   selector: 'app-footer',
@@ -7,9 +12,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  isPopUp: boolean = false;
+  isSuccess: boolean = false;
+
+  popupForm = this.fb.group({
+    name: ['', Validators.required],
+    phone: ['', [Validators.required]]
+  });
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
 
+  closePopUp() {
+    this.isPopUp = false;
+  }
+
+  openPopUp() {
+    this.isPopUp = true;
+    this.isSuccess = false;
+    this.popupForm.reset();
+  }
+
+  makeAppointment() {
+    if (this.popupForm.valid && this.popupForm.value.name && this.popupForm.value.phone) {
+      this.isSuccess = true;
+    }
+  }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterModule} from "@angular/router";
 import { ProductCardComponent } from './components/product-card/product-card.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     ProductCardComponent
   ],
-  imports: [
-    CommonModule,
-    RouterModule
-  ],
+    imports: [
+        CommonModule,
+        RouterModule,
+        FormsModule,
+        ReactiveFormsModule
+    ],
   exports: [
     ProductCardComponent
   ]
